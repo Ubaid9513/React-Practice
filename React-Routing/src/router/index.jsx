0aa944c27f6afb0ebdd29f6b91edf8ce1da4bf09
Ubaid9513 <shaikhubaid2345@gmail.com>
2025-01-15 03:08:31 +0500

    update router file

--- React-Routing/src/router/index.jsx
+++ React-Routing/src/router/index.jsx
@@ -2,15 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
 import Profile from "../Pages/Profile";
 import Login from "../Pages/Login";
+import AppLayout from "../components/Layout";
 
 function AppRouter() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/login" element={<AppLayout><Login/></AppLayout>} />
           <Route path="/" element={<Home/>} />
           <Route path="/profile" element={<Profile/>} />
-          <Route path="/login" element={<Login/>} />
         </Routes>
       </BrowserRouter>
     </>
